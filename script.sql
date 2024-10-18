2a0222e236d0f9a698807c33d59922d2f82b18a5
Said Polanco-Martagón <spolancom@upv.edu.mx>
2024-10-18 13:23:59 -0600

    se arreglo BD

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
-drop table alumnos:
+drop table alumnos;
 drop table profesores;
+
+create table profesores (
+	id int not null
+);
 
 create table alumnos (
 	id int not null primary key,
 	name varchar(20) not null
-);
-
-create table profesores (
-	id int not null
 );
