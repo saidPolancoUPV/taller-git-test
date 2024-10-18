9b53c60812596a9ae81ccd41a941b62f9a938356
Said Polanco-Martagón <spolancom@upv.edu.mx>
2024-10-18 13:07:58 -0600

    cambios a bd

---
+++ script.sql
@@ -0,0 +1,11 @@
+drop table alumnos:
+drop table profesores;
+
+create table alumnos (
+	id int not null primary key,
+	name varchar(20) not null
+);
+
+create table profesores (
+	id int not null
+);
